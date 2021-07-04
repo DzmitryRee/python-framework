b9396f2ced2dbb23dbeb7bf32768480f19143290
Dzmitry Vatutin <Dzmitry_Vatutsin@epam.com>
2021-07-05 01:00:48 +0300

    the legend

--- web/dummy_session.py
+++ web/dummy_session.py
@@ -7,12 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 try:
 	driver.get("https://www.youtube.com/")
 	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='search']")))
 	element = driver.find_element_by_xpath("//input[@id='search']")
-	element.send_keys('Rick')
+	element.send_keys('Rick r')
 	element.send_keys(Keys.RETURN)
 	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//a[@id="video-title"]')))
 	driver.find_element_by_xpath('//a[@id="video-title"]').click()
